7d9c69561061a2201e0d41a1c0c9ac765fc139c6
Mykola Herasymenko <nicksuper2000@gmail.com>
2024-07-08 04:02:28 -0400

    root

---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('I Am Rich'),
+        backgroundColor: Colors.grey[900],
+      ),
+    );
+  }
+}
